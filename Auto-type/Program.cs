4cd543398cf0af839873f6a3f97543db7a9f476a
Lucas Nykvist <lucasnykvist01@gmail.com>
2024-01-15 19:30:56 +0100

    Init

---
+++ Auto-type/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using System.Threading;
+
+class Program
+{
+    [STAThread]
+    static void Main()
+    {
+        string text = File.ReadAllText("text.txt");
+
+        Thread.Sleep(2000);
+
+        foreach (char c in text)
+        {
+            SendKeys.SendWait(c.ToString());
+        }
+
+        SendKeys.SendWait("{ENTER}");
+    }
+}
